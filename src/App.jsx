bd0d92c6da9fe9111d1fb6367761dd959183cf1d
Ilya Antonov <ilya.antonov@dreams-ai.com>
2024-07-02 13:40:33 +0100

    form data added for upload of photo

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ import {
   setupIonicReact,
 } from "@ionic/react";
 import {camera} from 'ionicons/icons'
+import FormData from 'form-data';
 
-/* Core CSS required for Ionic components to work properly */
 import './variables.css'
 import './App.css'
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
@@ -60,27 +60,23 @@ function App() {
 
   function sendMessage() {
     const message = `<b>Message to you:)</b>\n${text}`;
+    
+    const formData = new FormData();
+    formData.append('chat_id', CHAT_ID);
+    formData.append('parse_mode', 'HTML');
+
     try {
-      const encodedPhoto = base64ToBlob(photo || '')
-      axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`,
-        {
-          chat_id: CHAT_ID,
-          parse_mode: "HTML",
-          caption: message,
-          photo: [encodedPhoto, "failed-barcode.png"]
-        }
-      );
+      const encodedPhoto = base64ToBlob(photo || '');
+      formData.append('photo', encodedPhoto, 'photo-from-phone.png');
+      formData.append('caption', message);
+      axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, formData);
     }
     catch {
+      formData.append('text', message);
       axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
-        {
-          chat_id: CHAT_ID,
-          parse_mode: "HTML",
-          text: message,
-        }
+        formData
       );
     }
-
   }
 
   function dpns(e) {
